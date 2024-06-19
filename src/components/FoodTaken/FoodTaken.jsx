b8ee59681354e02ec5b62658c2f721803aa689ef
stevanitasya <stevanitasya499@gmail.com>
2024-06-19 22:03:11 +0700

    foodtaken

--- src/components/FoodTaken/FoodTaken.jsx
+++ src/components/FoodTaken/FoodTaken.jsx
@@ -4,21 +4,25 @@ import { useSelector } from "react-redux";
 import "./FoodTaken.css";
 
 const FoodTaken = () => {
-  const selectedItems = useSelector((state) => state.selectedItems);
+  const selectedItems = useSelector((state) => state.food.selectedItems || []);
 
   return (
-    <div className="food-taken"> 
+    <div className="food-taken">
       <div className="food-taken-text">Makanan yang dikonsumsi</div>
       <div className="food-choices-container">
-        {selectedItems.map((item) => (
-          <div key={item._id} className="food-choice-container2">
-            <img src={item.imageUrl} alt={item.name} className="food-image2" />
-            <div className="food-details">
-              <h3>{item.name}</h3>
-              <p>{item.calories} kkal {item.carbohydrates}g {item.protein}g {item.fat}g</p>
+        {selectedItems.length > 0 ? (
+          selectedItems.map((item) => (
+            <div key={item._id} className="food-choice-container2">
+              <img src={item.imageUrl} alt={item.name} className="food-image2" />
+              <div className="food-details">
+                <h3>{item.name}</h3>
+                <p>{item.calories} kkal {item.carbohydrates}g {item.protein}g {item.fat}g</p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        ) : (
+          <p>No food items found.</p>
+        )}
       </div>
     </div>
   );
