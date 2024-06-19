3cfb7cd85ef38885f0bed7e0f20d7c6b42610141
stevanitasya <stevanitasya499@gmail.com>
2024-06-19 23:39:57 +0700

    nutritiontrackingA

--- src/Pages/NutritionTracking/NutritionTrackingA/NutritionTrackingA.jsx
+++ src/Pages/NutritionTracking/NutritionTrackingA/NutritionTrackingA.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import FoodChoice from "../../../components/FoodChoice/FoodChoice";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const NutritionTrackingA = () => {
   const selectedItems = useSelector((state) => state.food.selectedItems);
+  const navigate = useNavigate();
 
   return (
     <div>
       <FoodChoice />
       <div>
         <h2>Selected Items: {selectedItems.length}</h2>
-        {navigate("/nutrition-tracking/food-choices/nutrition-data")};
+        {/* Use navigate in an event handler, not directly in the render */}
+        <button onClick={() => navigate("/nutrition-tracking/food-choices/nutrition-data")}>
+          Go to Nutrition Data
+        </button>
       </div>
     </div>
   );
