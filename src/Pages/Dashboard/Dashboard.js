77dc5ca9e197c17da4173938254ed5dc93622cb2
stevanitasya <stevanitasya499@gmail.com>
2024-06-20 18:52:38 +0700

    Dashboard

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
 import axios from "axios";
 import CollapseSideBar from "../../components/CollapseSideBar/CollapseSideBar";
 import Salad from "../../Assets/Salad.png";
@@ -9,28 +8,27 @@ import KalenderDashboard from "../../components/KalenderDashboard/KalenderDashbo
 import CalorieChart from "../../components/CalorieChart/CalorieChart";
 import NutritionDashboard from "../../components/NutritionDashboard/NutritionDashboard";
 import Header from "../../components/Header/Header";
-import { setFoodLogs, setNutritionSummary } from "../../redux/actions"; // Import actions
+import { setFoodLogs, setNutritionSummary } from "../../redux/actions";
 
 const Dashboard = () => {
   const dispatch = useDispatch();
-  const backendUrl = "http://localhost:5000"; // Use your backend URL
+  const backendUrl = process.env.REACT_APP_BACKEND_URL || "http://localhost:5000";
 
   useEffect(() => {
     const fetchUserData = async () => {
       try {
         const response = await axios.get(`${backendUrl}/api/users/dashboard?userid=666fd729a7d9380a07810628`);
-
         dispatch(setFoodLogs(response.data.foodLogs));
         dispatch(setNutritionSummary(response.data.nutritionSummary));
       } catch (error) {
-        console.error("Error fetching food logs:", error);
+        console.error("Error fetching user data:", error);
       }
     };
 
     fetchUserData();
   }, [dispatch, backendUrl]);
 
-  const { takenCalories, recommendedCalories } = useSelector((state) => state);
+  const { takenCalories, recommendedCalories } = useSelector((state) => state.nutritionSummary || {});
 
   return (
     <div className="App">
@@ -41,22 +39,34 @@ const Dashboard = () => {
           <div className="Dashboard-Content">
             <div className="Dashboard-Left">
               <div className="Dashboard-Req">
-                <img src={Salad} alt="DashboardSalad" className="DashboardSalad-img" />
-                <h1>Rekomendasi <br /> Makanan Hari ini.</h1>
+                <img
+                  src={Salad}
+                  alt="DashboardSalad"
+                  className="DashboardSalad-img"
+                />
+                <h1>
+                  Rekomendasi <br /> Makanan Hari ini.
+                </h1>
                 <Link to="/Recommendation">Lainnya...</Link>
               </div>
               <div className="Dashboard-Fitur">
                 <div className="Dashboard-Kalori">
                   <h1>Jumlah Kalori</h1>
                   <div className="Dashboard-Pengukur">
-                    <CalorieChart takenCalories={takenCalories} recommendedCalories={recommendedCalories} />
+                    <CalorieChart
+                      takenCalories={takenCalories || 0}
+                      recommendedCalories={recommendedCalories || 2000}
+                    />
                   </div>
                 </div>
                 <div className="Dashboard-Reminder">
                   <h1>Pengingat</h1>
                   <div className="spacing-br">
                     <div className="Dashboard-Pengingat">
-                      <h1>Sudahkah <br /> anda <br /> minum?</h1>
+                      <h1>
+                        Sudahkah <br /> anda
+                        <br /> minum?
+                      </h1>
                       <p>4 Liter/hari</p>
                     </div>
                   </div>
