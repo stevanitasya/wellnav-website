bfa8a60ddf73df802623e6d0e22fb1b1f523fa55
stevanitasya <stevanitasya499@gmail.com>
2024-06-28 14:31:59 +0700

    verifyemail

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import "../SignUpForm/SignUpForm.css";
 import { useNavigate } from "react-router-dom";
+import VerificationModal from './VerificationModal'; // Import modal
 
 const SignUpForm = () => {
+  const [showModal, setShowModal] = useState(false);
   const navigate = useNavigate();
   const backendUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5001';
 
@@ -37,13 +39,18 @@ const SignUpForm = () => {
       const response = await axios.post(`${backendUrl}/api/users/signup`, values);
       console.log(response.data);
       setStatus({ success: true });
-      navigate("/sign-in");
+      setShowModal(true); // Show modal after successful registration
       setSubmitting(false);
     } catch (error) {
       console.log(error.response.data);
       setErrors({ submit: error.message });
       setSubmitting(false);
     }
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    navigate("/sign-in");
   };
 
   const navigateToSignIn = () => {
@@ -59,112 +66,115 @@ const SignUpForm = () => {
   ];
 
   return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={validationSchema}
-      onSubmit={onSubmit}
-    >
-      {({ isSubmitting }) => (
-        <Form className="form">
-          <div className="input-row">
-            <div className="input-group-signup">
-              <Field
-                type="text"
-                id="username"
-                name="username"
-                placeholder="Username"
-                className="input-field"
-              />
-              <ErrorMessage name="username" component="div" className="error" />
+    <>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}
+      >
+        {({ isSubmitting }) => (
+          <Form className="form">
+            <div className="input-row">
+              <div className="input-group-signup">
+                <Field
+                  type="text"
+                  id="username"
+                  name="username"
+                  placeholder="Username"
+                  className="input-field"
+                />
+                <ErrorMessage name="username" component="div" className="error" />
+              </div>
+              <div className="input-group-signup">
+                <Field
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="Email"
+                  className="input-field"
+                />
+                <ErrorMessage name="email" component="div" className="error" />
+              </div>
             </div>
-            <div className="input-group-signup">
-              <Field
-                type="email"
-                id="email"
-                name="email"
-                placeholder="Email"
-                className="input-field"
-              />
-              <ErrorMessage name="email" component="div" className="error" />
+            <div className="input-row">
+              <div className="input-group-signup">
+                <Field
+                  type="password"
+                  id="password"
+                  name="password"
+                  placeholder="Password"
+                  className="input-field"
+                />
+                <ErrorMessage name="password" component="div" className="error" />
+              </div>
+              <div className="input-group-signup">
+                <Field
+                  type="password"
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  placeholder="Konfirmasi Password"
+                  className="input-field"
+                />
+                <ErrorMessage name="confirmPassword" component="div" className="error" />
+              </div>
             </div>
-          </div>
-          <div className="input-row">
-            <div className="input-group-signup">
-              <Field
-                type="password"
-                id="password"
-                name="password"
-                placeholder="Password"
-                className="input-field"
-              />
-              <ErrorMessage name="password" component="div" className="error" />
-            </div>
-            <div className="input-group-signup">
-              <Field
-                type="password"
-                id="confirmPassword"
-                name="confirmPassword"
-                placeholder="Konfirmasi Password"
-                className="input-field"
-              />
-              <ErrorMessage name="confirmPassword" component="div" className="error" />
-            </div>
-          </div>
-          <div className="input-row">
-            <div className="input-group-signup">
-              <Field
-                type="text"
-                id="age"
-                name="age"
-                placeholder="Umur"
-                className="input-field"
-              />
-              <ErrorMessage name="age" component="div" className="error" />
-            </div>
-            <div className="input-group-signup">
-              <Field
-                as="select"
-                id="healthCondition"
-                name="healthCondition"
-                className="input-field"
-              >
-                <option value="" disabled>
-                  Pilih kondisi kesehatan
-                </option>
-                {healthConditions.map((condition) => (
-                  <option key={condition} value={condition}>
-                    {condition}
+            <div className="input-row">
+              <div className="input-group-signup">
+                <Field
+                  type="text"
+                  id="age"
+                  name="age"
+                  placeholder="Umur"
+                  className="input-field"
+                />
+                <ErrorMessage name="age" component="div" className="error" />
+              </div>
+              <div className="input-group-signup">
+                <Field
+                  as="select"
+                  id="healthCondition"
+                  name="healthCondition"
+                  className="input-field"
+                >
+                  <option value="" disabled>
+                    Pilih kondisi kesehatan
                   </option>
-                ))}
-              </Field>
-              <ErrorMessage name="healthCondition" component="div" className="error" />
+                  {healthConditions.map((condition) => (
+                    <option key={condition} value={condition}>
+                      {condition}
+                    </option>
+                  ))}
+                </Field>
+                <ErrorMessage name="healthCondition" component="div" className="error" />
+              </div>
             </div>
-          </div>
-          <button
-            type="submit"
-            className="submit-button"
-            disabled={isSubmitting}
-          >
-            <h1>Daftar</h1>
-          </button>
-          <div className="signin-link">
-            <span>Sudah punya akun?</span>
             <button
-              type="button"
-              className="link-button"
-              onClick={navigateToSignIn}
+              type="submit"
+              className="submit-button"
+              disabled={isSubmitting}
             >
-              Masuk
+              <h1>Daftar</h1>
             </button>
-          </div>
-          <div className="login-methods">
-            <div className="login-method"></div>
-            <div className="login-method"></div>
-            <div className="login-method"></div>
-          </div>
-        </Form>
-      )}
-    </Formik>
+            <div className="signin-link">
+              <span>Sudah punya akun?</span>
+              <button
+                type="button"
+                className="link-button"
+                onClick={navigateToSignIn}
+              >
+                Masuk
+              </button>
+            </div>
+            <div className="login-methods">
+              <div className="login-method"></div>
+              <div className="login-method"></div>
+              <div className="login-method"></div>
+            </div>
+          </Form>
+        )}
+      </Formik>
+      <VerificationModal show={showModal} handleClose={handleCloseModal} />
+    </>
   );
 };
 
