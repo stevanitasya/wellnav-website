45f48acc8eadc820a07185b691b425a85fc72658
stevanitasya <stevanitasya499@gmail.com>
2024-08-17 15:52:49 +0700

    verify email

--- src/Pages/SignIn/SignIn.jsx
+++ src/Pages/SignIn/SignIn.jsx
@@ -5,6 +5,11 @@ import "../SignIn/SignIn.css";
 function SignIn() {
   return (
     <div>
+      {isVerified && (
+        <div className="verification-message">
+          <p>Email Anda telah berhasil diverifikasi. Silakan masuk.</p>
+        </div>
+      )}
       <h1 className="sign-in-title">Masuk</h1>
       <SignInForm />
     </div>
